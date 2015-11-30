817c421e7b567757690d13178700884b1e710f7f
Duke Oliver Kisch <dukekisch@gmail.com>
2015-11-30 13:42:26 -0500

    change request-list search bar text

--- theme.js
+++ theme.js
@@ -1,82 +1,84 @@
-/* jQuery v1.9.1 included
-*/
+ /* jQuery v1.9.1 included
+ */
 
 $(document).ready(function() {
+  
+   // Change text in search bar
+  $("#query").attr('placeholder','Search Our Knowledge Base');
+  
+  // change text in request-list search bar
+  $('#quick-search').attr('placeholder','Search Tickets');
 
-  // Change text in search bar
- $("#query").attr('placeholder','Browse Our Knowledge Base');
+  // toggle categories and sections on the home page
+  $(".category-tree").on("click", "h2 a, h3 a", function() {
+    $(this).parent().nextAll().toggle();
+    return false;
+  });
 
+  // social share popups
+  $(".share a").click(function(e) {
+    e.preventDefault();
+    window.open(this.href, "", "height = 500, width = 500");
+  });
 
- // toggle categories and sections on the home page
- $(".category-tree").on("click", "h2 a, h3 a", function() {
-   $(this).parent().nextAll().toggle();
-   return false;
- });
+  // toggle the share dropdown in communities
+  $(".share-label").on("click", function(e) {
+    e.stopPropagation();
+    var isSelected = this.getAttribute("aria-selected") == "true";
+    this.setAttribute("aria-selected", !isSelected);
+    $(".share-label").not(this).attr("aria-selected", "false");
+  });
 
- // social share popups
- $(".share a").click(function(e) {
-   e.preventDefault();
-   window.open(this.href, "", "height = 500, width = 500");
- });
+  $(document).on("click", function() {
+    $(".share-label").attr("aria-selected", "false");
+  });
 
- // toggle the share dropdown in communities
- $(".share-label").on("click", function(e) {
-   e.stopPropagation();
-   var isSelected = this.getAttribute("aria-selected") == "true";
-   this.setAttribute("aria-selected", !isSelected);
-   $(".share-label").not(this).attr("aria-selected", "false");
- });
+  // show form controls when the textarea receives focus or backbutton is used and value exists
+  var $answerbodyTextarea = $(".answer-body textarea"),
+      $answerFormControls = $(".answer-form-controls"),
+      $commentContainerTextarea = $(".comment-container textarea"),
+      $commentContainerFormControls = $(".comment-form-controls");
 
- $(document).on("click", function() {
-   $(".share-label").attr("aria-selected", "false");
- });
+  $answerbodyTextarea.one("focus", function() {
+    $answerFormControls.show();
+  });
 
- // show form controls when the textarea receives focus or backbutton is used and value exists
- var $answerbodyTextarea = $(".answer-body textarea"),
-     $answerFormControls = $(".answer-form-controls"),
-     $commentContainerTextarea = $(".comment-container textarea"),
-     $commentContainerFormControls = $(".comment-form-controls");
+  $commentContainerTextarea.one("focus", function() {
+    $commentContainerFormControls.show();
+  });
 
- $answerbodyTextarea.one("focus", function() {
-   $answerFormControls.show();
- });
+  if ($commentContainerTextarea.val() !== "") {
+    $commentContainerFormControls.show();
+  }
 
- $commentContainerTextarea.one("focus", function() {
-   $commentContainerFormControls.show();
- });
+  if ($answerbodyTextarea.val() !== "") {
+    $answerFormControls.show();
+  }
 
- if ($commentContainerTextarea.val() !== "") {
-   $commentContainerFormControls.show();
- }
+  // Submit requests filter form in the request list page
+  $("#request-status-select, #request-organization-select")
+    .on("change", function() {
+      search();
+    });
 
- if ($answerbodyTextarea.val() !== "") {
-   $answerFormControls.show();
- }
+  // Submit requests filter form in the request list page
+  $("#quick-search").on("keypress", function(e) {
+    if (e.which === 13) {
+      search();
+    }
+  });
 
- // Submit requests filter form in the request list page
- $("#request-status-select, #request-organization-select")
-   .on("change", function() {
-     search();
-   });
+  function search() {
+    window.location.search = $.param({
+      query: $("#quick-search").val(),
+      status: $("#request-status-select").val(),
+      organization_id: $("#request-organization-select").val()
+    });
+  }
 
- // Submit requests filter form in the request list page
- $("#quick-search").on("keypress", function(e) {
-   if (e.which === 13) {
-     search();
-   }
- });
-
- function search() {
-   window.location.search = $.param({
-     query: $("#quick-search").val(),
-     status: $("#request-status-select").val(),
-     organization_id: $("#request-organization-select").val()
-   });
- }
-
- // Submit organization form in the request page
- $("#request-organization select").on("change", function() {
-   this.form.submit();
- });
+  // Submit organization form in the request page
+  $("#request-organization select").on("change", function() {
+    this.form.submit();
+  });
 
 });
